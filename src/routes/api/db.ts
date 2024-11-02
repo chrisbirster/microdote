c36d71bb0245fccda87ffa6ee8bcc502c38f0df0
Chris Birster <christopher.birster@gmail.com>
2024-11-02 12:15:17 -0400

    handle db error

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@libsql/client";
 
 export const db = createClient({
-  url: process.env.DB_URL ?? "db://url",
+  url: process.env.DB_URL ?? "libsql://url",
   authToken: process.env.DB_TOKEN ?? "some_auth_token",
 });
