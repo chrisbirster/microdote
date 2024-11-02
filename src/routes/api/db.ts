e698ff43983536a96619138ceaa7c3b27503e262
Chris Birster <christopher.birster@gmail.com>
2024-11-02 13:25:54 -0400

    env

--- src/routes/api/db.ts
+++ src/routes/api/db.ts
@@ -1,8 +1,6 @@
 import { createClient } from "@libsql/client";
-import { serverEnv } from "~/env/server";
-import { clientEnv } from "~/env/client";
 
 export const db = createClient({
-  url: clientEnv.DB_URL,
-  authToken: clientEnv.DB_TOKEN,
+  url: process.env.DB_URL,
+  authToken: process.env.DB_TOKEN,
 });
